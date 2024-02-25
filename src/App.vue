db71bc3dda906f9d828732134ad6423e3d0993b6
GabrielAdelino <gabrielbradelino@gmail.com>
2024-02-25 19:42:35 -0300

    Working on inputs

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,50 @@
 <script setup>
+import { ref, onMounted, computed, watch } from 'vue'
 
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const inoput_category = ref(null)
+
+const todos_asc = computed(() => todos.value((a, b) => {
+return a.createAt - b.createAt
+}))
+
+const addTodo = () => {}
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+    name.value = localStorage.getItem('name') || ''
+})
 </script>
 
 <template>
-<div>
-  Hello World
-</div>
+
+  <main class="app">
+
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="texte" placeholder="Name here" 
+        v-model="name" >
+      </h2>
+    </section>
+
+  <section class="create-todo">
+    <h3>CREATE A TODO</h3>
+
+    <form @submit.prevent="addTodo">
+          <h4>What's on your todo list?</h4>
+          <input type="text" placeholder="e.g. make a video" 
+          v-model="input_content">
+
+          {{ input_content }}
+    </form>
+  </section>
+  </main>
+
 </template>
 
